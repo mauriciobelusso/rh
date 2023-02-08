e1bb71921096fecf43f1b5b0bd1266a2df014c1f
mauriciobelusso <mauricio.belusso@portoseguro.com.br>
2023-02-08 10:18:12 -0300

    Documentada a classe de configuração do security

--- src/main/java/mauriciobelusso/com/github/rh/configuration/SecurityConfiguration.java
+++ src/main/java/mauriciobelusso/com/github/rh/configuration/SecurityConfiguration.java
@@ -16,20 +16,28 @@ public class SecurityConfiguration {
     @Bean
     @SneakyThrows
     public SecurityFilterChain filterChain(HttpSecurity http) {
+        // desativa a proteção de segurança do "frame options header".
+        // Esse header é usado para proteger contra o ataque de "clickjacking"
         http.headers().frameOptions().disable();
 
+        // Habilitado o CORS e desabilitado o CSRF por ser um projeto apenas de desenvolvimento
         http.cors()
                 .and().csrf().disable()
                 .authorizeRequests()
+                    // Liberadas as rotas para poder
+                    // apresentar o form default do Spring Security
+                    // e também poder acessar o console do H2
                     .antMatchers( "/error/**").permitAll()
                     .antMatchers( "/login/**").permitAll()
                     .antMatchers("/h2-console/**").permitAll()
                     .anyRequest().authenticated()
                     .and()
+                // Permite usar o Form de login default do Spring Security no navegador
                 .formLogin().permitAll()
                     .and()
                 .logout().permitAll()
                     .and()
+                // Permite o uso da autenticação básica
                 .httpBasic(withDefaults());;
 
         return http.build();
