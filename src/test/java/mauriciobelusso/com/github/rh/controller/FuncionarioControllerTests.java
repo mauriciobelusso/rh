f62ff4853c1c428c5c8803db61a7a3ce30b613dc
mauriciobelusso <mauricio.belusso@portoseguro.com.br>
2023-02-08 01:54:49 -0300

    Criado o controller para funcionario e os testes unitários

--- src/test/java/mauriciobelusso/com/github/rh/controller/FuncionarioControllerTests.java
+++ src/test/java/mauriciobelusso/com/github/rh/controller/FuncionarioControllerTests.java
@@ -6,10 +6,10 @@ import mauriciobelusso.com.github.rh.service.FuncionarioService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -23,6 +23,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(FuncionarioController.class)
+@AutoConfigureMockMvc(addFilters = false)
 public class FuncionarioControllerTests {
 
     @Autowired
